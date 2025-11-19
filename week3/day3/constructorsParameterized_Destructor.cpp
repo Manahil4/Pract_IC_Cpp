d1336ad3cb9427e8b3008eead5cee5801d192aec
Manahil4 <manahilejaz16@gmail.com>
2025-11-19 22:34:17 +0500

    day3

--- week3/day3/constructorsParameterized_Destructor.cpp
+++ week3/day3/constructorsParameterized_Destructor.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class DOG{
+    string name;
+    int age;
+public:
+DOG(string n, int a){
+    name = n;
+    age = a;
+    cout << "Dog's Name: " << name << ", Age: " << age << " years." << endl;
+}
+~DOG(){
+    cout << "Destructor called for dog: " << name << endl;
+}
+};
 class Book{
     int price; int pages; int rating; string title; string author;
 public:
@@ -22,10 +35,14 @@ Book(){
              << "\nRating: " << rating << endl;
     
     }  // Parameterized constructor
-
+~Book(){
+    cout << "Destructor called for book: " << title << endl;
+}
 };
 int main(){
     Book b1, b2;  // Object creation which invokes parameterized constructor
-
+DOG d1("Buddy", 3);
+DOG d2("Max", 5);
+    return 0;
 
 }
